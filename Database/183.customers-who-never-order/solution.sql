1ba2e72150b15f2722323fa45482ef1fb5f5ded1
nikolagavranovic <76073360+nikolagavranovic@users.noreply.github.com>
2023-01-13 09:20:33 +0100

    Solution for exercise 183

---
+++ Database/183.customers-who-never-order/solution.sql
@@ -0,0 +1,64 @@
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- +-------------+---------+
+-- id is the primary key column for this table.
+-- Each row of this table indicates the ID and name of a customer.
+
+
+-- Table: Orders
+
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | id          | int  |
+-- | customerId  | int  |
+-- +-------------+------+
+-- id is the primary key column for this table.
+-- customerId is a foreign key of the ID from the Customers table.
+-- Each row of this table indicates the ID of an order and the ID of the customer
+
+-- Write an SQL query to report all customers who never order anything.
+
+-- Return the result table in any order.
+
+-- The query result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Customers table:
+-- +----+-------+
+-- | id | name  |
+-- +----+-------+
+-- | 1  | Joe   |
+-- | 2  | Henry |
+-- | 3  | Sam   |
+-- | 4  | Max   |
+-- +----+-------+
+-- Orders table:
+-- +----+------------+
+-- | id | customerId |
+-- +----+------------+
+-- | 1  | 3          |
+-- | 2  | 1          |
+-- +----+------------+
+-- Output: 
+-- +-----------+
+-- | Customers |
+-- +-----------+
+-- | Henry     |
+-- | Max       |
+-- +-----------+
+
+
+select Customers.name as Customers
+from Customers
+left join Orders
+on Customers.id = Orders.customerId
+where customerId is null
